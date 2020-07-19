b4aafc34ac500c68af6465d9ee02208704fee4c6
ranmiella <67534530+ranmiella@users.noreply.github.com>
2020-07-19 07:49:31 +0700

    Update __main__.py

--- SaitamaRobot/__main__.py
+++ SaitamaRobot/__main__.py
@@ -18,7 +18,7 @@ from SaitamaRobot.modules.helper_funcs.misc import paginate_modules
 
 PM_START_TEXT = """
 Hi {}, my name is {}! 
-I am an Anime themed group management bot.
+wkwkwktestasdasda
 To add me to your group click ["HERE"](t.me/SaitamaRobot?startgroup=botstart)
 You can find my list of available commands with /help.
 
