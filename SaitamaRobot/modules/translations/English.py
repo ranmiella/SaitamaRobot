2d67876e0f19e27db488fafd22af02e2e969582b
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-07-20 00:29:22 +0530

    Update English.py

--- SaitamaRobot/modules/translations/English.py
+++ SaitamaRobot/modules/translations/English.py
@@ -212,18 +212,7 @@ or feature requests you might have :)
 If you enjoying using me and/or would like to help me survive in the wild, hit /donate to help fund/upgrade my VPS!
 """,
 
-    "send-help": """Alexa is a extremely modular and easy to use telegram bot loaded with all the essential commands that a group administrator needs 
-
-*NOTE :*
-Most of the commands of  @RealAlexaBot are available for admins so dont panic if some command isn't working (that's probably because you are not admin in the chat). However irrespective of this anyone can use the bot in PM
-
-@RealAlexaBot depends completely on admin permission rather than it's own so if you are missing any certain rights then it will simply ignore you !
-
-*Basic Commands :*
-/start: starts the bot
-/help: gives the available list of commands
-/runs: check if bot is alive or not
-/ping: check ping response
+    "send-help": """Hello I am a group management bot made by this sexy girl ==> @Taelha
 """,
 
 #Misc
